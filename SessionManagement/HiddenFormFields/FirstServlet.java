5b00c03dd79ce77a10f0ea4014952996833a80d0
Vaseem Naazleen Shaik <54474853+vnaazleen@users.noreply.github.com>
2021-12-29 11:49:34 +0530

    Add files via upload

---
+++ SessionManagement/HiddenFormFields/FirstServlet.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+public class FirstServlet extends HttpServlet {
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		String user = request.getParameter("user");
+		out.println("<h1>" + user + "</h1>");
+		out.println("<form action=SecondServlet><input type=hidden name=user value=" + user + ">");
+		out.println("<button type=submit>Submit</button></form>");
+		out.close();
+	}
+}
